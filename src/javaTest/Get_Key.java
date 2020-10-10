12346c74af00cae1db5d77b474b16f88f4a186c5
George-Tseng <georgecycuphy@cycu.org.tw>
2020-10-10 21:15:23 +0800

    Third Modify
    2020-10-10
    1.提供Console隱密輸入功能
    2.提供第三種轉換方式(使用到質數)
    3.隱蔽部分資訊
    4.其他修正

--- src/javaTest/Get_Key.java
+++ src/javaTest/Get_Key.java
@@ -68,9 +68,13 @@ public class Get_Key {
         String timeStart = Get_Date.getDateNow();
         String timeEnd;
 
+        /*字串轉換*/
+        String inputParaT = ASCII_Translator.getMessageASCII(inputTime);
+        String inputParaM = ASCII_Translator.getMessageASCII(inputMacAddress);
+
         /*用split拆裝String*/
-        String [] timeSpace = inputTime.split(",");
-        String [] macAddressSpace = inputMacAddress.split(",");
+        String [] timeSpace = inputParaT.split(",");
+        String [] macAddressSpace = inputParaM.split(",");
         String [] textSpace = inputText.split(",");
 
         /*參數*/
@@ -189,9 +193,13 @@ public class Get_Key {
         String timeStart = Get_Date.getDateNow();
         String timeEnd;
 
+        /*字串轉換*/
+        String inputParaT = ASCII_Translator.getMessageASCII(inputTime);
+        String inputParaM = ASCII_Translator.getMessageASCII(inputMacAddress);
+
         /*用split拆裝String*/
-        String [] timeSpace = inputTime.split(",");
-        String [] macAddressSpace = inputMacAddress.split(",");
+        String [] timeSpace = inputParaT.split(",");
+        String [] macAddressSpace = inputParaM.split(",");
         String [] textSpace = inputText.split(",");
 
         /*參數*/
@@ -245,6 +253,158 @@ public class Get_Key {
         timeEnd = Get_Date.getDateNow();
         System.out.println(Get_Date.getTimeDifference(timeStart, timeEnd));
 
+        return sb0.toString();
+    }
+
+    protected static String getEncodeKeyLong(String inputTime, String inputMacAddress, String inputKey) throws ParseException {
+        /*計時用*/
+        String timeStart = Get_Date.getDateNow();
+        String timeEnd;
+
+        /*用split拆裝String*/
+        String [] timeSpace = inputTime.split(",");
+        String [] macAddressSpace = inputMacAddress.split(",");
+
+        /*用toCharArray方法把匯入的String拆成Char陣列*/
+        char [] keySpace = inputKey.toCharArray();
+
+        /*參數*/
+        int paraTime = Integer.parseInt(timeSpace[0]);
+        int paraMacAddress = Integer.parseInt(macAddressSpace[0]);
+
+        /*運算產物*/
+        long paraResult, paraRest;
+
+        /*生成StringBuilder物件*/
+        StringBuilder sb0 = new StringBuilder();
+
+        /*利用迴圈執行轉換*/
+        for(int i = 0; i < keySpace.length; i++){
+            int paraInputTmp = ASCII_Translator.getASCII(keySpace[i]);
+            int paraInputPrime = Get_Prime_Number.getMaxPrimeNumI(paraInputTmp);
+            int paraInput = paraInputPrime * 10 + (paraInputTmp - paraInputPrime);
+
+            int paraT, paraM;
+            long paraTotalUp, paraTotalDown;
+
+            if(keySpace.length <= paraTime){
+                int paraTTmp = Integer.parseInt(timeSpace[i + 1]);
+                int paraTPrime = Get_Prime_Number.getMaxPrimeNumI(paraTTmp);
+                paraT = paraTPrime * 10 + (paraTTmp - paraTPrime);
+            }
+            else{
+                int paraTTmp = Integer.parseInt(timeSpace[i % paraTime + 1]);
+                int paraTPrime = Get_Prime_Number.getMaxPrimeNumI(paraTTmp);
+                paraT = paraTPrime * 10 + (paraTTmp - paraTPrime);
+            }
+
+            if(keySpace.length <= paraMacAddress){
+                int paraMTmp = Integer.parseInt(macAddressSpace[i + 1]);
+                int paraMPrime = Get_Prime_Number.getMaxPrimeNumI(paraMTmp);
+                paraM = paraMPrime * 10 + (paraMTmp - paraMPrime);
+            }
+            else{
+                int paraMTmp = Integer.parseInt(macAddressSpace[i % paraMacAddress + 1]);
+                int paraMPrime = Get_Prime_Number.getMaxPrimeNumI(paraMTmp);
+                paraM = paraMPrime * 10 + (paraMTmp - paraMPrime);
+            }
+
+            long paraTmp = (long)(Math.pow((paraMacAddress * paraM), 2) - Math.pow(paraInput, 2));
+            paraTotalUp = (long)((2 * (paraTime * paraT) * Math.pow((paraMacAddress * paraM), 2) * paraInput)
+                    + (2 * paraInput + 1) * (paraTime * paraT) * paraTmp
+                    + (Math.pow(paraInput, 2) * (paraTime * paraT) * paraTmp));
+            paraTotalDown = paraMacAddress * paraM * paraInput * (paraTmp);
+
+            paraResult = paraTotalUp / paraTotalDown;
+            paraRest = paraTotalUp % paraTotalDown + paraResult;
+
+            sb0.append(paraResult);
+            sb0.append(":");
+            sb0.append(Long.toHexString(paraRest));
+            if(i < keySpace.length - 1){
+                sb0.append(",");
+            }
+        }
+
+        timeEnd = Get_Date.getDateNow();
+        System.out.println(Get_Date.getTimeDifference(timeStart, timeEnd));
+        return sb0.toString();
+    }
+
+    protected static String getDecodeKeyLong(String inputTime, String inputMacAddress, String inputText) throws ParseException {
+        /*計時用*/
+        String timeStart = Get_Date.getDateNow();
+        String timeEnd;
+
+        /*字串轉換*/
+        String inputParaT = ASCII_Translator.getMessageASCII(inputTime);
+        String inputParaM = ASCII_Translator.getMessageASCII(inputMacAddress);
+
+        /*用split拆裝String*/
+        String [] timeSpace = inputParaT.split(",");
+        String [] macAddressSpace = inputParaM.split(",");
+        String [] textSpace = inputText.split(",");
+
+        /*參數*/
+        int paraTime = Integer.parseInt(timeSpace[0]);
+        int paraMacAddress = Integer.parseInt(macAddressSpace[0]);
+
+        /*反運算參數*/
+        long paraResult, paraRest;
+
+        /*生成StringBuilder物件*/
+        StringBuilder sb0 = new StringBuilder();
+
+        /*利用迴圈執行轉換*/
+        for(int i = 0; i < textSpace.length; i++){
+            /*用split拆裝String*/
+            String [] keySpace = textSpace[i].split(":");
+
+            int paraT, paraM;
+            long paraTotalUp, paraTotalDown;
+
+            if (textSpace.length <= paraTime) {
+                int paraTTmp = Integer.parseInt(timeSpace[i + 1]);
+                int paraTPrime = Get_Prime_Number.getMaxPrimeNumI(paraTTmp);
+                paraT = paraTPrime * 10 + (paraTTmp - paraTPrime);
+            } else {
+                int paraTTmp = Integer.parseInt(timeSpace[i % paraTime + 1]);
+                int paraTPrime = Get_Prime_Number.getMaxPrimeNumI(paraTTmp);
+                paraT = paraTPrime * 10 + (paraTTmp - paraTPrime);
+            }
+
+            if (textSpace.length <= paraMacAddress) {
+                int paraMTmp = Integer.parseInt(macAddressSpace[i + 1]);
+                int paraMPrime = Get_Prime_Number.getMaxPrimeNumI(paraMTmp);
+                paraM = paraMPrime * 10 + (paraMTmp - paraMPrime);
+            } else {
+                int paraMTmp = Integer.parseInt(macAddressSpace[i % paraMacAddress + 1]);
+                int paraMPrime = Get_Prime_Number.getMaxPrimeNumI(paraMTmp);
+                paraM = paraMPrime * 10 + (paraMTmp - paraMPrime);
+            }
+
+            paraResult = Long.parseLong(keySpace[0]);
+            paraRest = Long.valueOf(keySpace[1], 16) - paraResult;
+
+            for(int j = 0; j < 128; j++){
+                int jPrime = Get_Prime_Number.getMaxPrimeNumI(j);
+                int jTmp = jPrime * 10 + (j - jPrime);
+                long paraTmp = (long)(Math.pow((paraMacAddress * paraM), 2) - Math.pow(jTmp, 2));
+                paraTotalUp = (long)((2 * (paraTime * paraT) * Math.pow((paraMacAddress * paraM), 2) * jTmp)
+                        + (2 * jTmp + 1) * (paraTime * paraT) * paraTmp
+                        + (Math.pow(jTmp, 2) * (paraTime * paraT) * paraTmp));
+                paraTotalDown = paraMacAddress * paraM * jTmp * (paraTmp);
+
+                if(paraTotalDown != 0){
+                    if((paraResult == paraTotalUp / paraTotalDown) && (paraRest == paraTotalUp % paraTotalDown)){
+                        sb0.append(ASCII_Translator.getChar(j));
+                        break;
+                    }
+                }
+            }
+        }
+        timeEnd = Get_Date.getDateNow();
+        System.out.println(Get_Date.getTimeDifference(timeStart, timeEnd));
         return sb0.toString();
     }
 }
--- src/javaTest/Java_ASCII_Test.java
+++ src/javaTest/Java_ASCII_Test.java
@@ -1,53 +1,103 @@
 package javaTest;
 
+import java.io.Console;
 import java.text.ParseException;
 import java.util.Scanner;
 
 public class Java_ASCII_Test {
 	public static void main(String[] args) throws ParseException {
 
-		Scanner inputScan = new Scanner(System.in);
-		
-		String myTime = Get_Date.getDateNow();
-		System.out.println("現在時間為："+myTime);
-		String myMacAddress = Get_Mac_Address.getMacAddress().equals("") ? "NotFound" : Get_Mac_Address.getMacAddress();
-		System.out.println("網卡位址為："+myMacAddress);
+		String myTime, myMacAddress, encodeTime, encodeMacAddress, decodeTime, decodeMacAddress, inputKey, exitKey;
 
-		String encodeTime = ASCII_Translator.getMessageASCII(myTime);
-		String encodeMacAddress = ASCII_Translator.getMessageASCII(myMacAddress);
-		System.out.println("轉換後時間資訊為："+encodeTime);
-		System.out.println("轉換後網卡資訊為："+encodeMacAddress);
+		if(System.console() == null){
+			Scanner inputScan = new Scanner(System.in);
 
-		String decodeTime = ASCII_Translator.getMessageChar(encodeTime);
-		String decodeMacAddress = ASCII_Translator.getMessageChar(encodeMacAddress);
-		System.out.println("轉換回時間資訊為："+decodeTime);
-		System.out.println("轉換回網卡資訊為："+decodeMacAddress);
+			myTime = Get_Date.getDateNow();
+			System.out.println("現在時間為："+myTime);
+			myMacAddress = Get_Mac_Address.getMacAddress().equals("") ? "NotFound" : Get_Mac_Address.getMacAddress();
+			System.out.println("網卡位址為："+myMacAddress);
 
-		while(true) {
-			System.out.println("請輸入密文(限英文):");
-			String inputKey = inputScan.nextLine();
+			encodeTime = ASCII_Translator.getMessageASCII(myTime);
+			encodeMacAddress = ASCII_Translator.getMessageASCII(myMacAddress);
+			decodeTime = ASCII_Translator.getMessageChar(encodeTime);
+			decodeMacAddress = ASCII_Translator.getMessageChar(encodeMacAddress);
 
-			String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
-			System.out.println("轉換結果為：" + result);
+			while(true) {
+				System.out.print("請輸入密文(限英文):");
 
-			String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
-			System.out.println("轉換結果為：" + resultMid);
+				inputKey = inputScan.nextLine();
 
-			String recoveryKey = Get_Key.getDecodeKey(encodeTime, encodeMacAddress, result);
-			System.out.println("還原結果為：" + recoveryKey);
+				String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
+				System.out.println("方法一轉換結果為：" + result);
 
-			String recoveryKeyMid = Get_Key.getDecodeKeyMid(encodeTime, encodeMacAddress, resultMid);
-			System.out.println("還原結果為：" + recoveryKeyMid);
-			
-			System.out.println("\n如要結束程式，請輸入「Exit」:");
-			String exitKey = inputScan.nextLine();
-			
-			if(exitKey.toUpperCase().equals("EXIT")) {
-				System.out.println("程式已結束...");
-				break;
+				String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
+				System.out.println("方法二轉換結果為：" + resultMid);
+
+				String resultLong = Get_Key.getEncodeKeyLong(encodeTime, encodeMacAddress, inputKey);
+				System.out.println("方法三轉換結果為：" + resultLong);
+
+				String recoveryKey = Get_Key.getDecodeKey(decodeTime, decodeMacAddress, result);
+				System.out.println("方法一還原結果為：" + recoveryKey);
+
+				String recoveryKeyMid = Get_Key.getDecodeKeyMid(decodeTime, decodeMacAddress, resultMid);
+				System.out.println("方法二還原結果為：" + recoveryKeyMid);
+
+				String recoveryKeyLong = Get_Key.getDecodeKeyLong(decodeTime, decodeMacAddress, resultLong);
+				System.out.println("方法三還原結果為：" + recoveryKeyLong);
+
+				System.out.println("\n如要結束程式，請輸入「Exit」:");
+				exitKey = inputScan.nextLine();
+				if(exitKey.toUpperCase().equals("EXIT")) {
+					System.out.println("程式已結束...");
+					break;
+				}
 			}
+			inputScan.close();
 		}
-		
-		inputScan.close();
+		else{
+			Console inputCon = System.console();
+			Scanner consoleScan = new Scanner(inputCon.reader());
+
+			myTime = Get_Date.getDateNow();
+			System.out.println("現在時間為："+myTime);
+			myMacAddress = Get_Mac_Address.getMacAddress().equals("") ? "NotFound" : Get_Mac_Address.getMacAddress();
+			System.out.println("網卡位址為："+myMacAddress);
+
+			encodeTime = ASCII_Translator.getMessageASCII(myTime);
+			encodeMacAddress = ASCII_Translator.getMessageASCII(myMacAddress);
+			decodeTime = ASCII_Translator.getMessageChar(encodeTime);
+			decodeMacAddress = ASCII_Translator.getMessageChar(encodeMacAddress);
+
+			while(true){
+				char [] inputKeyChar = inputCon.readPassword("請輸入密文(限英文):");
+				inputKey = new String(inputKeyChar);
+
+				String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
+				System.out.println("方法一轉換結果為：" + result);
+
+				String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
+				System.out.println("方法二轉換結果為：" + resultMid);
+
+				String resultLong = Get_Key.getEncodeKeyLong(encodeTime, encodeMacAddress, inputKey);
+				System.out.println("方法三轉換結果為：" + resultLong);
+
+				String recoveryKey = Get_Key.getDecodeKey(decodeTime, decodeMacAddress, result);
+				System.out.println("方法一還原結果為：" + recoveryKey);
+
+				String recoveryKeyMid = Get_Key.getDecodeKeyMid(decodeTime, decodeMacAddress, resultMid);
+				System.out.println("方法二還原結果為：" + recoveryKeyMid);
+
+				String recoveryKeyLong = Get_Key.getDecodeKeyLong(decodeTime, decodeMacAddress, resultLong);
+				System.out.println("方法三還原結果為：" + recoveryKeyLong);
+
+				System.out.println("\n如要結束程式，請輸入「Exit」:");
+				exitKey = consoleScan.nextLine();
+				if(exitKey.toUpperCase().equals("EXIT")) {
+					System.out.println("程式已結束...");
+					break;
+				}
+			}
+			consoleScan.close();
+		}
 	}
 }
