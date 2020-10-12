55769e9aa48ccaab116f3d86983c37a83e3ebd79
George-Tseng <57084949+George-Tseng@users.noreply.github.com>
2020-10-12 12:58:14 +0800

    Fourth Modifty
    2020-10-12
    1.去除部分未使用的方法
    2.避免在無法取得必要資訊下執行內容轉換

--- src/javaTest/Java_ASCII_Test.java
+++ src/javaTest/Java_ASCII_Test.java
@@ -18,32 +18,37 @@ public class Java_ASCII_Test {
 			System.out.println("網卡位址為："+myMacAddress);
 
 			encodeTime = ASCII_Translator.getMessageASCII(myTime);
-			encodeMacAddress = ASCII_Translator.getMessageASCII(myMacAddress);
+			encodeMacAddress = (myMacAddress.equals("NotFound")) ? null : ASCII_Translator.getMessageASCII(myMacAddress);
 			decodeTime = ASCII_Translator.getMessageChar(encodeTime);
-			decodeMacAddress = ASCII_Translator.getMessageChar(encodeMacAddress);
+			decodeMacAddress = (myMacAddress.equals("NotFound")) ? null : ASCII_Translator.getMessageChar(encodeMacAddress);
 
 			while(true) {
 				System.out.print("請輸入密文(限英文):");
 
 				inputKey = inputScan.nextLine();
 
-				String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
-				System.out.println("方法一轉換結果為：" + result);
+				if(!myMacAddress.equals("NotFound")){
+					String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
+					System.out.println("方法一轉換結果為：" + result);
 
-				String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
-				System.out.println("方法二轉換結果為：" + resultMid);
+					String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
+					System.out.println("方法二轉換結果為：" + resultMid);
 
-				String resultLong = Get_Key.getEncodeKeyLong(encodeTime, encodeMacAddress, inputKey);
-				System.out.println("方法三轉換結果為：" + resultLong);
+					String resultLong = Get_Key.getEncodeKeyLong(encodeTime, encodeMacAddress, inputKey);
+					System.out.println("方法三轉換結果為：" + resultLong);
 
-				String recoveryKey = Get_Key.getDecodeKey(decodeTime, decodeMacAddress, result);
-				System.out.println("方法一還原結果為：" + recoveryKey);
+					String recoveryKey = Get_Key.getDecodeKey(decodeTime, decodeMacAddress, result);
+					System.out.println("方法一還原結果為：" + recoveryKey);
 
-				String recoveryKeyMid = Get_Key.getDecodeKeyMid(decodeTime, decodeMacAddress, resultMid);
-				System.out.println("方法二還原結果為：" + recoveryKeyMid);
+					String recoveryKeyMid = Get_Key.getDecodeKeyMid(decodeTime, decodeMacAddress, resultMid);
+					System.out.println("方法二還原結果為：" + recoveryKeyMid);
 
-				String recoveryKeyLong = Get_Key.getDecodeKeyLong(decodeTime, decodeMacAddress, resultLong);
-				System.out.println("方法三還原結果為：" + recoveryKeyLong);
+					String recoveryKeyLong = Get_Key.getDecodeKeyLong(decodeTime, decodeMacAddress, resultLong);
+					System.out.println("方法三還原結果為：" + recoveryKeyLong);
+				}
+				else{
+					System.out.println("無法取得網卡資訊，請確認網路連線狀態正常後重開本程式...");
+				}
 
 				System.out.println("\n如要結束程式，請輸入「Exit」:");
 				exitKey = inputScan.nextLine();
@@ -64,31 +69,36 @@ public class Java_ASCII_Test {
 			System.out.println("網卡位址為："+myMacAddress);
 
 			encodeTime = ASCII_Translator.getMessageASCII(myTime);
-			encodeMacAddress = ASCII_Translator.getMessageASCII(myMacAddress);
+			encodeMacAddress = (myMacAddress.equals("NotFound")) ? null : ASCII_Translator.getMessageASCII(myMacAddress);
 			decodeTime = ASCII_Translator.getMessageChar(encodeTime);
-			decodeMacAddress = ASCII_Translator.getMessageChar(encodeMacAddress);
+			decodeMacAddress = (myMacAddress.equals("NotFound")) ? null : ASCII_Translator.getMessageChar(encodeMacAddress);
 
 			while(true){
 				char [] inputKeyChar = inputCon.readPassword("請輸入密文(限英文):");
 				inputKey = new String(inputKeyChar);
 
-				String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
-				System.out.println("方法一轉換結果為：" + result);
+				if(!myMacAddress.equals("NotFound")){
+					String result = Get_Key.getEncodeKey(encodeTime, encodeMacAddress, inputKey);
+					System.out.println("方法一轉換結果為：" + result);
 
-				String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
-				System.out.println("方法二轉換結果為：" + resultMid);
+					String resultMid = Get_Key.getEncodeKeyMid(encodeTime, encodeMacAddress, inputKey);
+					System.out.println("方法二轉換結果為：" + resultMid);
 
-				String resultLong = Get_Key.getEncodeKeyLong(encodeTime, encodeMacAddress, inputKey);
-				System.out.println("方法三轉換結果為：" + resultLong);
+					String resultLong = Get_Key.getEncodeKeyLong(encodeTime, encodeMacAddress, inputKey);
+					System.out.println("方法三轉換結果為：" + resultLong);
 
-				String recoveryKey = Get_Key.getDecodeKey(decodeTime, decodeMacAddress, result);
-				System.out.println("方法一還原結果為：" + recoveryKey);
+					String recoveryKey = Get_Key.getDecodeKey(decodeTime, decodeMacAddress, result);
+					System.out.println("方法一還原結果為：" + recoveryKey);
 
-				String recoveryKeyMid = Get_Key.getDecodeKeyMid(decodeTime, decodeMacAddress, resultMid);
-				System.out.println("方法二還原結果為：" + recoveryKeyMid);
+					String recoveryKeyMid = Get_Key.getDecodeKeyMid(decodeTime, decodeMacAddress, resultMid);
+					System.out.println("方法二還原結果為：" + recoveryKeyMid);
 
-				String recoveryKeyLong = Get_Key.getDecodeKeyLong(decodeTime, decodeMacAddress, resultLong);
-				System.out.println("方法三還原結果為：" + recoveryKeyLong);
+					String recoveryKeyLong = Get_Key.getDecodeKeyLong(decodeTime, decodeMacAddress, resultLong);
+					System.out.println("方法三還原結果為：" + recoveryKeyLong);
+				}
+				else{
+					System.out.println("無法取得網卡資訊，請確認網路連線狀態正常後重開本程式...");
+				}
 
 				System.out.println("\n如要結束程式，請輸入「Exit」:");
 				exitKey = consoleScan.nextLine();
